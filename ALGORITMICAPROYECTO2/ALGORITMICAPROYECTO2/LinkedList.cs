2c8c3a9d7a7cae36ab090ec76f273567b263637e
pablin2402 <pabloperedo04@gmail.com>
2020-04-24 12:13:24 -0400

    Delete,Update and Add Hash Operations

---
+++ ALGORITMICAPROYECTO2/ALGORITMICAPROYECTO2/LinkedList.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALGORITMICAPROYECTO2
+{
+    class LinkedList
+    {
+        public class Nodo
+        {
+            public int info;
+            public int id;
+            public Nodo ant, sig;
+        }
+
+        Nodo raiz;
+        public LinkedList()
+        {
+            raiz = null;
+        }
+        //INSERTAR AL PRINCIPIO
+
+        public void InsertarPrimero(int x, int id)
+        {
+            Nodo nuevo = new Nodo();
+            nuevo.info = x;
+            nuevo.id = id;
+            nuevo.sig = raiz;
+
+            if (raiz != null)
+                raiz.ant = nuevo;
+            raiz = nuevo;
+        }
+        public void InsertarFinal(int x)
+        {
+
+            Nodo nuevo = new Nodo();
+            nuevo.info = x;
+            if (raiz == null)
+                raiz = nuevo;
+            else
+            {
+                Nodo reco = raiz;
+                while (reco.sig != null)
+                {
+                    reco = reco.sig;
+                }
+                reco.sig = nuevo;
+                nuevo.ant = reco;
+            }
+
+
+        }
+
+        //INSERTAR EN POSICION
+        public void insertar(int pos, int x)
+        {
+            if (pos <= Cantidad() + 1)
+            {
+                Nodo nuevo = new Nodo();
+                nuevo.info = x;
+               
+                if (pos == 1)
+                {
+                    nuevo.sig = raiz;
+                    raiz = nuevo;
+                }
+                else
+                    if (pos == Cantidad() + 1)
+                {
+                    Nodo reco = raiz;
+                    while (reco.sig != null)
+                    {
+                        reco = reco.sig;
+                    }
+                    reco.sig = nuevo;
+                    nuevo.sig = null;
+                }
+                else
+                {
+                    Nodo reco = raiz;
+                    for (int f = 1; f <= pos - 2; f++)
+                        reco = reco.sig;
+                    Nodo siguiente = reco.sig;
+                    reco.sig = nuevo;
+                    nuevo.sig = siguiente;
+                }
+            }
+        }
+        public void Borrar(int pos)
+        {
+            if (pos <= Cantidad())
+            {
+                if (pos == 1)
+                {
+                    raiz = raiz.sig;
+                    if (raiz != null)
+                        raiz.ant = null;
+                }
+                else
+                {
+                    Nodo reco;
+                    reco = raiz;
+                    for (int f = 1; f <= pos - 2; f++)
+                        reco = reco.sig;
+                    Nodo prox = reco.sig;
+                    prox = prox.sig;
+                    reco.sig = prox;
+                    if (prox != null)
+                        prox.ant = reco;
+                }
+            }
+        }
+
+
+
+
+        public int Extraer(int pos)
+        {
+            if (pos <= Cantidad())
+            {
+                int informacion;
+                if (pos == 1)
+                {
+                    informacion = raiz.info;
+                    raiz = raiz.sig;
+                    if (raiz != null)
+                        raiz.ant = null;
+                }
+                else
+                {
+                    Nodo reco;
+                    reco = raiz;
+                    for (int f = 1; f <= pos - 2; f++)
+                        reco = reco.sig;
+                    Nodo prox = reco.sig;
+                    reco.sig = prox.sig;
+                    Nodo siguiente = prox.sig;
+                    if (siguiente != null)
+                        siguiente.ant = reco;
+                    informacion = prox.info;
+                }
+                return informacion;
+            }
+            else
+                return int.MaxValue;
+        }
+
+
+
+        public void Intercambiar(int pos1, int pos2)
+        {
+            if (pos1 <= Cantidad() && pos2 <= Cantidad())
+            {
+                Nodo reco1 = raiz;
+                for (int f = 1; f < pos1; f++)
+                    reco1 = reco1.sig;
+                Nodo reco2 = raiz;
+                for (int f = 1; f < pos2; f++)
+                    reco2 = reco2.sig;
+                int aux = reco1.info;
+                reco1.info = reco2.info;
+                reco2.info = aux;
+            }
+        }
+
+
+        public int Cantidad()
+        {
+            int cant = 0;
+            Nodo reco = raiz;
+            while (reco != null)
+            {
+                reco = reco.sig;
+                cant++;
+            }
+            return cant;
+        }
+
+
+
+        public bool Existe(int x)
+        {
+            Nodo reco = raiz;
+            while (reco != null)
+            {
+                if (reco.info == x)
+                    return true;
+                reco = reco.sig;
+            }
+            return false;
+        }
+
+        public bool Vacia()
+        {
+            if (raiz == null)
+                return true;
+            else
+                return false;
+        }
+
+        public void Imprimir()
+        {
+            Nodo reco = raiz;
+            while (reco != null)
+            {
+                Console.Write(reco.info + "-");
+                reco = reco.sig;
+            }
+
+        }
+    }
+}
---
+++ ALGORITMICAPROYECTO2/ALGORITMICAPROYECTO2/Paciente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ALGORITMICAPROYECTO2
+{
+    enum estado
+    {
+        vacio, ocupado, borrado
+    }
+    public class Paciente 
+    {
+        private int llave;
+        private string valor;
+
+        private estado miEstado;
+
+        //ahora las propiedades de cada uno de los atributos declarados
+        public int Llave { get => llave; set => llave = value; }
+        public string Valor { get => valor; set => valor = value; }
+        internal estado MiEstado { get => miEstado; set => miEstado = value; }
+
+        public Paciente()
+        {
+            llave = 0;
+            valor = "";
+            miEstado = estado.vacio; //valor definido en el enum
+        }
+    }
+}
