0b46c77e33ab579265fa5ab0594b03e6ae3fb424
Frosty9810 <42982241+Frosty9810@users.noreply.github.com>
2020-04-24 13:47:31 -0400

    Adding Binary Search on LinkedList

---
+++ BINARY_LINKED-LISTS/TESTING/LinkedList.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoAlgoritmica
+{
+    public class LinkedList
+    {
+
+        public class Nodo
+        {
+            public int info;
+            public Nodo ant, sig;
+        }
+
+        Nodo raiz;
+        public LinkedList()
+        {
+            raiz = null;
+        }
+        //INSERTAR AL PRINCIPIO
+
+        public void InsertarPrimero(int x)
+        {
+            Nodo nuevo = new Nodo();
+            nuevo.info = x;
+            nuevo.sig = raiz;
+            if (raiz != null)
+                raiz.ant = nuevo;
+            raiz = nuevo;
+        }
+        public void InsertarFinal(int x)
+        {
+
+            if (raiz == null)
+            {
+                raiz = new Nodo();
+                raiz.info = x;
+                raiz.sig = null;
+
+            }
+            else
+            {
+
+                Nodo nuevo = new Nodo();
+                nuevo.info = x;
+                Nodo actual = raiz;
+                while (actual.sig != null)
+                {
+                    actual = actual.sig;
+
+                }
+                actual.sig = nuevo;
+            }
+
+
+        }
+
+        //INSERTAR EN POSICION
+        public void insertar(int pos, int x)
+        {
+            if (pos <= Cantidad() + 1)
+            {
+                Nodo nuevo = new Nodo();
+                nuevo.info = x;
+                if (pos == 1)
+                {
+                    nuevo.sig = raiz;
+                    raiz = nuevo;
+                }
+                else
+                    if (pos == Cantidad() + 1)
+                {
+                    Nodo reco = raiz;
+                    while (reco.sig != null)
+                    {
+                        reco = reco.sig;
+                    }
+                    reco.sig = nuevo;
+                    nuevo.sig = null;
+                }
+                else
+                {
+                    Nodo reco = raiz;
+                    for (int f = 1; f <= pos - 2; f++)
+                        reco = reco.sig;
+                    Nodo siguiente = reco.sig;
+                    reco.sig = nuevo;
+                    nuevo.sig = siguiente;
+                }
+            }
+        }
+
+
+
+
+        public int Extraer(int pos)
+        {
+            if (pos <= Cantidad())
+            {
+                int informacion;
+                if (pos == 1)
+                {
+                    informacion = raiz.info;
+                    raiz = raiz.sig;
+                    if (raiz != null)
+                        raiz.ant = null;
+                }
+                else
+                {
+                    Nodo reco;
+                    reco = raiz;
+                    for (int f = 1; f <= pos - 2; f++)
+                        reco = reco.sig;
+                    Nodo prox = reco.sig;
+                    reco.sig = prox.sig;
+                    Nodo siguiente = prox.sig;
+                    if (siguiente != null)
+                        siguiente.ant = reco;
+                    informacion = prox.info;
+                }
+                return informacion;
+            }
+            else
+                return int.MaxValue;
+        }
+
+        public void Borrar(int pos)
+        {
+            if (pos <= Cantidad())
+            {
+                if (pos == 1)
+                {
+                    raiz = raiz.sig;
+                    if (raiz != null)
+                        raiz.ant = null;
+                }
+                else
+                {
+                    Nodo reco;
+                    reco = raiz;
+                    for (int f = 1; f <= pos - 2; f++)
+                        reco = reco.sig;
+                    Nodo prox = reco.sig;
+                    prox = prox.sig;
+                    reco.sig = prox;
+                    if (prox != null)
+                        prox.ant = reco;
+                }
+            }
+        }
+
+        public void Intercambiar(int pos1, int pos2)
+        {
+            if (pos1 <= Cantidad() && pos2 <= Cantidad())
+            {
+                Nodo reco1 = raiz;
+                for (int f = 1; f < pos1; f++)
+                    reco1 = reco1.sig;
+                Nodo reco2 = raiz;
+                for (int f = 1; f < pos2; f++)
+                    reco2 = reco2.sig;
+                int aux = reco1.info;
+                reco1.info = reco2.info;
+                reco2.info = aux;
+            }
+        }
+
+        public int Mayor()
+        {
+            if (!Vacia())
+            {
+                int may = raiz.info;
+                Nodo reco = raiz.sig;
+                while (reco != null)
+                {
+                    if (reco.info > may)
+                        may = reco.info;
+                    reco = reco.sig;
+                }
+                return may;
+            }
+            else
+                return int.MaxValue;
+        }
+
+        public int PosMayor()
+        {
+            if (!Vacia())
+            {
+                int may = raiz.info;
+                int x = 1;
+                int pos = x;
+                Nodo reco = raiz.sig;
+                while (reco != null)
+                {
+                    if (reco.info > may)
+                    {
+                        may = reco.info;
+                        pos = x;
+                    }
+                    reco = reco.sig;
+                    x++;
+                }
+                return pos;
+            }
+            else
+                return int.MaxValue;
+        }
+
+        public int Cantidad()
+        {
+            int cant = 0;
+            Nodo reco = raiz;
+            while (reco != null)
+            {
+                reco = reco.sig;
+                cant++;
+            }
+            return cant;
+        }
+
+        public bool Ordenada()
+        {
+            if (Cantidad() > 1)
+            {
+                Nodo reco1 = raiz;
+                Nodo reco2 = raiz.sig;
+                while (reco2 != null)
+                {
+                    if (reco2.info < reco1.info)
+                    {
+                        return false;
+                    }
+                    reco2 = reco2.sig;
+                    reco1 = reco1.sig;
+                }
+            }
+            return true;
+        }
+
+        public bool Existe(int x)
+        {
+            Nodo reco = raiz;
+            while (reco != null)
+            {
+                if (reco.info == x)
+                    return true;
+                reco = reco.sig;
+            }
+            return false;
+        }
+
+        public bool Vacia()
+        {
+            if (raiz == null)
+                return true;
+            else
+                return false;
+        }
+
+        public void Imprimir()
+        {
+            Nodo reco = raiz;
+            while (reco != null)
+            {
+                Console.Write(reco.info + "-");
+                reco = reco.sig;
+            }
+
+        }
+
+    }
+}
---
+++ BINARY_LINKED-LISTS/TESTING/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+ 
+public class Node
+
+    
+{
+    public int data;
+    public Node next;
+
+    // Constructor para crear el nodo
+    public Node(int d)
+    {
+        
+        data = d;
+        next = null;
+    }
+}
+
+class BinarySearch
+{
+    // Funcion para insertar un valor al principio de una linked list
+    
+    static Node push(Node head, int data)
+    {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+        return head;
+    }
+
+    // Funcion para encontrar el nodo del medio. 
+    // usando punterso Slow y Fast donde slow avanza la lista cada 1 nodo y Fast avanza la lista cada 2 nodos.
+    static Node middleNode(Node start, Node last)
+    {
+        if (start == null)
+            return null;
+
+        Node slow = start;
+        Node fast = start.next;
+
+        while (fast != last)
+        {
+            fast = fast.next;
+            if (fast != last)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+        }
+        return slow;
+    }
+
+    
+    static Node binarySearch(Node head, int value)
+    {
+        Node start = head;
+        Node last = null;
+
+        do
+        {
+            // Encontrar el medio
+            Node mid = middleNode(start, last);
+
+            // Si el medio esta vacio
+            if (mid == null)
+                return null;
+
+            // Si el valor esta en el meedio 
+            if (mid.data == value)
+                return mid;
+
+            // Si el medio es mayor al valor
+            else if (mid.data > value)
+            {
+                start = mid.next;
+            }
+
+            // Si el valor es mayor al del medio 
+            else
+                last = mid;
+        } while (last == null || last != start);
+
+        //si no existe. 
+        return null;
+    }
+    
+    public static void Main(String[] args)
+    {
+        Node head = null;
+
+
+        head = push(head, 1);
+        head = push(head, 4);
+        head = push(head, 7);
+        head = push(head, 8);
+        head = push(head, 9);
+        head = push(head, 10);
+        int value = 7;
+
+        if (binarySearch(head, value) == null)
+        {
+            Console.WriteLine("Valor no presente");
+        }
+        else
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+
+
+}
